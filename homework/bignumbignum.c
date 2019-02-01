b3228b96e85970db63dc47c137a9ebe2cdb466fb
Oscar Vian Valles <oscarvianvalles@gmail.com>
2019-02-01 08:15:59 +0800

    new code from gab

--- homework/bignumbignum.c
+++ homework/bignumbignum.c
@@ -1,146 +1,83 @@
-#include <stdio.h>
+/*  BIGNUMBIGNUM >>> PRINTING THE 1000th FIBONACCI NUMBER USING ARRAYS
+*   COLLABORATORS:  
+*
+*
+*/
+#include<stdio.h>
 
-int main() {
-    int n = 10, arraySize = 20;
-    int arrayA[arraySize], arrayB[arraySize], arrayC[arraySize];
-    int index[4] = {arraySize - 1, arraySize - 1, arraySize - 1, arraySize - 1},
-        indexLimit = 0, end = 0;
-    int sum = 0, carry = 0;
-    int initNumber = 0;
+int main()
+{
+    int i, j, sum, index;
+    int size = 1000, n = 1000;
+    int arrA[size], arrB[size], arrC[size];
 
-    // Initializing Numbers
-    for (int i = 0; i < arraySize; i++) {
-        arrayA[i] = 0;
-        arrayB[i] = 0;
-        arrayC[i] = 0;
+    // initializing arrays
+    for(i = 0; i < size; i++)
+    {
+        /* code */
+        arrA[i] = arrB[i] = arrC[i] = 0;
     }
 
-    // Initilaizing numbers to the array
-    initNumber = 199;
+    // initial value for the fibonacci sequence
+    arrB[size-1] = 1;
 
-    index[0] = arraySize - 1;
-    while (initNumber != 0) {
-        arrayA[index[0]--] = initNumber % 10;
-        initNumber /= 10;
-    }
+    // loop for the nth fibonacci
+    for(i = 1; i < n; i++)
+    {
+        /* code */
+        // reset third array
+        arrC[i] = 0;
 
-    initNumber = 1;
-    index[1] = arraySize - 1;
-    while (initNumber != 0) {
-        arrayB[index[1]--] = initNumber % 10;
-        initNumber /= 10;
-    }
-
-    if (index[0] < index[1]) {
-        indexLimit = index[0];
-    } else {
-        indexLimit = index[1];
-    }
-
-    for (int x = 0; x < arraySize; x++) {
-        printf("%i", arrayA[x]);
-    }
-    printf("\n");
-
-    for (int x = 0; x < arraySize; x++) {
-        printf("%i", arrayB[x]);
-    }
-    printf("\n");
-
-    do {
-        // Sums the two index,
-        sum = arrayA[index[2]] + arrayB[index[2]];
-        index[2]--;
-
-        if (sum >= 10) {
-            arrayA[index[2]] += sum / 10;
-            sum %= 10;
+        // loop for the addition
+        for(j = size-1; j >= 0; j--)
+        {
+            /* code */
+            sum = arrA[j] + arrB[j];
+            // checks for possible carry
+            if (sum < 10) {
+                /* code */
+                arrC[j] = sum;
+            }
+            else {
+                /* code */
+                // getting the least sig digit
+                arrC[j] = sum % 10;
+                // getting the carry
+                arrA[j-1] += sum / 10;
+            }
         }
 
-        arrayC[index[3]--] = sum;
+        // lastUsedC = j;
 
-        if (index[2] == indexLimit) {
-            end = 1;
+        // loop for the swap
+        for(j = size-1; j >= 0; j--)
+        {
+            /* code */
+            // a = b
+            arrA[j] = arrB[j];
+            // b = c
+            arrB[j] = arrC[j];
         }
-    } while (!end);
-
-    for (int x = 0; x < arraySize; x++) {
-        printf("%i", arrayC[x]);
     }
+    
+    // printing
+    // for(i = 0; i < size; i++)
+    // {
+    //     /* code */
+    //     // checks for trailing 6 zeroes
+    //     if (arrA[i] == 0 && arrA[i+1] == 0 && arrA[i+2] == 0 && arrA[i+3] == 0 && arrA[i+4] == 0 && arrA[i+5])
+    //         index = i + 5;
+    //     // printf("%i", arrA[i]);
+    // }
+
+    printf("The %i fibonacci number is: ", n);
+
+    // print
+    for(i = 0; i < size; i++)
+    {
+        /* code */
+        printf("%i", arrA[i]);
+    }
+    
     printf("\n");
-
-    // for (int i = 0; i < n; i++) {
-    //     // Reset arrayC
-
-    //     /*----------------------*
-    //      *       c = a + b      *
-    //      * ---------------------*/
-
-    //     for (int i = 0; i < arraySize; i++) {
-    //         arrayC[i] = 0;
-    //     }
-
-    // // Reset Index
-    // index[2] = arraySize - 1;
-    // index[3] = arraySize - 1;
-
-    //     do {
-    //         // Sums the two index,
-    //         sum = arrayA[index[2]] + arrayB[index[2]];
-    //         index[2]--;
-
-    //         while (sum != 0) {
-    //             if (sum > 10) {
-    //                 carry = 1;
-    //             }
-    //             arrayC[index[3]--] += sum % 10;
-    //             sum /= 10;
-    //         }
-
-    //         if (carry) {
-    //             index[3]++;
-    //             carry = 0;
-    //         }
-
-    //         if (index[2] == indexLimit) {
-    //             end = 1;
-    //         }
-    //     } while (!end);
-
-    //     /*----------------------*
-    //      *         a = b        *
-    //      * ---------------------*/
-
-    //     // Reset Index & copy arrayB
-    //     indexLimit = index[1];
-    //     for (int i = arraySize - 1; i > index[1]; i--) {
-    //         arrayA[i] = arrayB[i];
-    //     }
-
-    //     /*----------------------*
-    //      *         b = c        *
-    //      * ---------------------*/
-
-    //     // Reset Index & copy arrayB
-    //     for (int i = 0; i < arraySize; i++) {
-    //         arrayB[i] = arrayC[i];
-    //     }
-    // }
-
-    // for (int x = 0; x < arraySize; x++) {
-    //     printf("%i", arrayA[x]);
-    // }
-    // printf("\n");
-
-    // for (int x = 0; x < arraySize; x++) {
-    //     printf("%i", arrayB[x]);
-    // }
-    // printf("\n");
-
-    // for (int x = 0; x < arraySize; x++) {
-    //     printf("%i", arrayC[x]);
-    // }
-    // printf("\n");
-
-    return 0;
 }
