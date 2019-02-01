51ca2330302ac779333117bdb9dc611f5fc706d9
Oscar Vian Valles <oscarvianvalles@gmail.com>
2019-02-01 14:15:57 +0800

    cleanup code

--- homework/bignumbignum.c
+++ homework/bignumbignum.c
@@ -1,83 +1,60 @@
 /*  BIGNUMBIGNUM >>> PRINTING THE 1000th FIBONACCI NUMBER USING ARRAYS
-*   COLLABORATORS:  
-*
-*
-*/
-#include<stdio.h>
+ *   COLLABORATORS:
+ *
+ *
+ */
+#include <stdio.h>
 
-int main()
-{
-    int i, j, sum, index;
+int main() {
+    int i, j, sum;
     int size = 1000, n = 1000;
     int arrA[size], arrB[size], arrC[size];
 
     // initializing arrays
-    for(i = 0; i < size; i++)
-    {
-        /* code */
+    for (i = 0; i < size; i++) {
         arrA[i] = arrB[i] = arrC[i] = 0;
     }
 
     // initial value for the fibonacci sequence
-    arrB[size-1] = 1;
+    arrA[size - 1] = 0;
+    arrB[size - 1] = 1;
+
 
     // loop for the nth fibonacci
-    for(i = 1; i < n; i++)
-    {
-        /* code */
+    for (i = 1; i < n; i++) {
         // reset third array
-        arrC[i] = 0;
+        for (int i = size - 1; i >= 0; i--) {
+            arrC[i] = 0;
+        }
 
         // loop for the addition
-        for(j = size-1; j >= 0; j--)
-        {
-            /* code */
+        for (j = size - 1; j >= 0; j--) {
             sum = arrA[j] + arrB[j];
             // checks for possible carry
-            if (sum < 10) {
-                /* code */
-                arrC[j] = sum;
+            if (sum > 10) {
+                arrA[j - 1] += sum / 10;
+                sum %= 10;
             }
-            else {
-                /* code */
-                // getting the least sig digit
-                arrC[j] = sum % 10;
-                // getting the carry
-                arrA[j-1] += sum / 10;
-            }
+            arrC[j] = sum;
         }
 
         // lastUsedC = j;
 
         // loop for the swap
-        for(j = size-1; j >= 0; j--)
-        {
-            /* code */
+        for (j = size - 1; j >= 0; j--) {
             // a = b
             arrA[j] = arrB[j];
             // b = c
             arrB[j] = arrC[j];
         }
     }
-    
-    // printing
-    // for(i = 0; i < size; i++)
-    // {
-    //     /* code */
-    //     // checks for trailing 6 zeroes
-    //     if (arrA[i] == 0 && arrA[i+1] == 0 && arrA[i+2] == 0 && arrA[i+3] == 0 && arrA[i+4] == 0 && arrA[i+5])
-    //         index = i + 5;
-    //     // printf("%i", arrA[i]);
-    // }
 
     printf("The %i fibonacci number is: ", n);
 
     // print
-    for(i = 0; i < size; i++)
-    {
-        /* code */
+    for (i = 0; i < size; i++) {
         printf("%i", arrA[i]);
     }
-    
+
     printf("\n");
 }
