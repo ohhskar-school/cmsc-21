9b2c0dca81820900ad441b65bf0164a91a703df9
Oscar Vian Valles <oscarvianvalles@gmail.com>
2019-02-01 18:15:58 +0800

    added digit checking

--- homework/bignumbignum.c
+++ homework/bignumbignum.c
@@ -7,11 +7,10 @@
 
 int main() {
     int i, j, sum;
-    int size = 1000, n = 1000;
+    int size = 1000, n = 0;
     int arrA[size], arrB[size], arrC[size];
-    int indexA, indexB, indexC = indexB = indexA = 0;
-    int lastUsedA, lastUsedB, lastUsedC = lastUsedB = lastUsedA = 0;
     int digits = 1;
+    int notFit = 0;
 
     // initializing arrays
     for (i = 0; i < size; i++) {
@@ -21,8 +20,14 @@ int main() {
     // initial value for the fibonacci sequence
     arrA[size - 1] = 0;
     arrB[size - 1] = 1;
-    lastUsedA = size - 2;
-    lastUsedB = size - 2;
+
+    do {
+        if (n < 0) {
+            printf("Please enter a non-negative number\n");
+        }
+        printf("Enter a number as n to find the nths Fibonacci Number\n");
+        scanf("%i", &n);
+    } while (n < 0);
 
     // loop for the nth fibonacci
     for (i = 1; i < n; i++) {
@@ -35,18 +40,19 @@ int main() {
         for (j = size - 1; j >= size - digits - 1; j--) {
             sum = arrA[j] + arrB[j];
             // checks for possible carry
-            if (sum > 10) {
+            if (sum < 10) {
+                arrC[j] = sum;
+            } else {
+                arrC[j] = sum % 10;
                 arrA[j - 1] += sum / 10;
-                sum %= 10;
                 if (j == size - digits) {
                     digits++;
                 }
             }
-            arrC[j] = sum;
         }
-        
+
         // loop for the swap
-        for (j = size - 1; j >= size - digits - 1; j--) {
+        for (j = size - 1; j >= 0; j--) {
             // a = b
             arrA[j] = arrB[j];
             // b = c
@@ -54,11 +60,41 @@ int main() {
         }
     }
 
-    printf("The %i fibonacci number is: ", n);
+    char suffix[2] = "th";
+    if (!(n % 100 > 10 && n % 100 < 20)) {
+        switch (n % 10) {
+            case 1:
+                suffix[0] = 's';
+                suffix[1] = 't';
+                break;
+
+            case 2:
+                suffix[0] = 'n';
+                suffix[1] = 'd';
+                break;
+
+            case 3:
+                suffix[0] = 'r';
+                suffix[1] = 'd';
+                break;
+
+            default:
+                break;
+        }
+    }
 
     // print
-    for (i = size - digits; i < size ; i++) {
-        printf("%i", arrA[i]);
+    if (digits > 1000) {
+        printf(
+            "The %i%s element in the Fibonacci Sequence will not fit in a 1000 "
+            "digit array\n",
+            n, suffix);
+    } else {
+        printf("The %i%s element in the Fibonacci Sequence is: ", n, suffix);
+
+        for (i = size - digits; i < size; i++) {
+            printf("%i", arrC[i]);
+        }
     }
 
     printf("\n");
