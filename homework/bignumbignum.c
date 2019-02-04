9131c70da6b5dbe16fa156cb94c3700c4275ad91
Oscar Vian Valles <oscarvianvalles@gmail.com>
2019-02-04 13:11:50 +0800

    added working bignum

--- homework/bignumbignum.c
+++ homework/bignumbignum.c
@@ -1,83 +1,79 @@
-/*  BIGNUMBIGNUM >>> PRINTING THE 1000th FIBONACCI NUMBER USING ARRAYS
-*   COLLABORATORS:  
-*
-*
-*/
-#include<stdio.h>
-
-int main()
-{
-    int i, j, sum, index;
-    int size = 1000, n = 1000;
+// BIGNUM BIGNUM
+// Collaborators:      Justin Andre Po
+//                     Gabriel John Manzanares
+//                     Oscar Vian Valles
+//                     Seth Nemeño
+#include <stdio.h>
+int main() {
+    int sum, i, j, k, n = 0, size = 1000, digits = 1, mod = 1;
     int arrA[size], arrB[size], arrC[size];
+    char suffix[2] = "th";
 
-    // initializing arrays
-    for(i = 0; i < size; i++)
-    {
-        /* code */
-        arrA[i] = arrB[i] = arrC[i] = 0;
+    for (i = 0; i < size; i++)
+        arrA[i] = arrB[i] = arrC[i] = 0;  // initialize the arrays
+
+    arrB[size - 1] = 1;  // initialization for the fibonacci sequence
+
+    do {
+        if (n < 0) printf("Please enter a non-negative number.\n");
+        printf("Enter a number as n to find the nths Fibonaccin Number.\n");
+        scanf("%i", &n);
+    } while (n < 0);  // checks for negative input
+
+    if (n != 2) {
+        n--;
+        mod = 0;
     }
 
-    // initial value for the fibonacci sequence
-    arrB[size-1] = 1;
-
-    // loop for the nth fibonacci
-    for(i = 1; i < n; i++)
-    {
-        /* code */
-        // reset third array
-        arrC[i] = 0;
-
-        // loop for the addition
-        for(j = size-1; j >= 0; j--)
-        {
-            /* code */
+    for (i = 1; i < n; i++) {
+        for (k = size - 1; k >= 0; k--) arrC[k] = 0;  // resets the third array
+        // add values of the arrays
+        for (j = size - 1; j >= size - digits - 1; j--) {
             sum = arrA[j] + arrB[j];
             // checks for possible carry
-            if (sum < 10) {
-                /* code */
+            if (sum < 10)
                 arrC[j] = sum;
-            }
             else {
-                /* code */
-                // getting the least sig digit
                 arrC[j] = sum % 10;
-                // getting the carry
-                arrA[j-1] += sum / 10;
+                arrA[j - 1] += sum / 10;
+                if ((j == size - digits))
+                    digits++;  // counts the number of digits
             }
         }
-
-        // lastUsedC = j;
-
-        // loop for the swap
-        for(j = size-1; j >= 0; j--)
-        {
-            /* code */
-            // a = b
+        // swap values of arrays
+        for (j = size - 1; j >= 0; j--) {
             arrA[j] = arrB[j];
-            // b = c
             arrB[j] = arrC[j];
         }
     }
-    
-    // printing
-    // for(i = 0; i < size; i++)
-    // {
-    //     /* code */
-    //     // checks for trailing 6 zeroes
-    //     if (arrA[i] == 0 && arrA[i+1] == 0 && arrA[i+2] == 0 && arrA[i+3] == 0 && arrA[i+4] == 0 && arrA[i+5])
-    //         index = i + 5;
-    //     // printf("%i", arrA[i]);
-    // }
-
-    printf("The %i fibonacci number is: ", n);
-
-    // print
-    for(i = 0; i < size; i++)
-    {
-        /* code */
-        printf("%i", arrA[i]);
+    // suffix
+    n++;
+    if (mod){
+        n = 2;
     }
-    
+    if (!(n % 100 > 10 && n % 100 < 20)) {
+        switch (n % 10) {
+            case 1:
+                suffix[0] = 's';
+                suffix[1] = 't';
+                break;
+            case 2:
+                suffix[0] = 'n';
+                suffix[1] = 'd';
+                break;
+            case 3:
+                suffix[0] = 'r';
+                suffix[1] = 'd';
+                break;
+        }
+    }
+    // printing
+    printf("The %i%s element in the Fibonacci Sequence ", n, suffix);
+    if (digits > 1000)
+        printf("will not fit in a 1000-digit array.\n");
+    else {
+        printf("is: ");
+        for (i = size - digits; i < size; i++) printf("%i", arrC[i]);
+    }
     printf("\n");
 }
