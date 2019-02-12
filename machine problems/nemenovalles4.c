6c65b237fd2ed9f9f8ac71af6854df3aabf826e3
Oscar Vian Valles <oscarvianvalles@gmail.com>
2019-02-12 14:50:31 +0800

    added documentation and better error checking

--- machine problems/nemenovalles4.c
+++ machine problems/nemenovalles4.c
@@ -3,7 +3,7 @@
 void printDayOfTheWeek(int, int, int);
 void printNthDay(int, int, int);
 void case2(int, int);
-void printDayOfTheWeekFromNth(int, int);
+int checkInputs(int day, int month, int year, int testCaseType);
 int checkLeapYear(int);
 int roundOwn(float);
 
@@ -43,8 +43,12 @@ int main() {
             printf("\nEnter the Year\n");
             scanf("%i", &year);
 
-            printDayOfTheWeek(day, month, year);
-            printNthDay(day, month, year);
+            if (!checkInputs(day, month, year, testCaseType)) {
+                printDayOfTheWeek(day, month, year);
+                printNthDay(day, month, year);
+            } else {
+                printf("INVALID\n");
+            }
         }
 
         // TestCaseType == 2
@@ -55,7 +59,11 @@ int main() {
             printf("\nEnter the Year\n");
             scanf("%i", &year);
 
-            case2(day, year);
+            if (!checkInputs(day, 1, year, testCaseType)) {
+                case2(day, year);
+            } else {
+                printf("INVALID\n");
+            }
         }
     }
 
@@ -84,14 +92,30 @@ void printDayOfTheWeek(int day, int month, int year) {
 void printNthDay(int day, int month, int year) {
     int table[2][13] = {{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
                         {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
+    // There is a zeroth month so that when accessing the table 1 corresponds to
+    // january, which is what is the common way to represent the month in
+    // numbers.
+
+    // leapYear is a flag that states if the year given is a leap year through
+    // the check leap year function
     int leapYear = checkLeapYear(year), nthDay = 0;
 
+    // Loops through the loop until it reaches the month BEFORE the indicated
+    // test case
     for (int i = 0; i < month; i++) {
         nthDay += table[leapYear][i];
     }
+
+    // Adds the day given in the test case to the days that was accumulated from
+    // the previous months
     nthDay += day;
+
+    // Creates the suffix based on the last 2 digits
     char suffix[] = "th";
+
+    // Runs this switch if the last two digits are within 00 - 09 and 20 - 99
     if (!(nthDay % 100 > 10 && nthDay % 100 < 20)) {
+        // Checks the last two digits if it is 1,2, or 3
         switch (nthDay % 10) {
             case 1:
                 suffix[0] = 's';
@@ -104,6 +128,8 @@ void printNthDay(int day, int month, int year) {
             case 3:
                 suffix[0] = 'r';
                 suffix[1] = 'd';
+                break;
+            default:
                 break;
         }
     }
@@ -118,9 +144,11 @@ void case2(int day, int year) {
                          "April",   "May",       "June",     "July",
                          "August",  "September", "October",  "November",
                          "December"};
+    int leapYear = checkLeapYear(year), iterator = 1;
 
-    int leapYear = checkLeapYear(year), iterator = 0;
-
+    // Runs the loop while day is greater than the months in the table. This
+    // means that it stops when the day is found within the month. The iterator
+    // will be the month.
     while (day - table[leapYear][iterator] > 0) {
         day -= table[leapYear][iterator];
         iterator++;
@@ -130,14 +158,57 @@ void case2(int day, int year) {
     printf("%s %i\n", months[iterator], day);
 }
 
+int checkInputs(int day, int month, int year, int testCaseType) {
+    int table[2][13] = {{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+                        {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
+    int leapYear = checkLeapYear(year);
+
+    // Determines the number of days to check depending on the leap year
+
+    // If year is negative return an error
+    if (year < 0) {
+        return 1;
+    }
+
+    // Checks the test case type to know what to check.
+    if (testCaseType == 1) {
+        // Checks if the day given is greater than the month that it should
+        // have, depending if it is a leap year or not
+        if (day > table[leapYear][month]) {
+            return 1;
+        }
+    } else {
+        int dayCheck = leapYear ? 366 : 365;
+
+        // Checks if month is less than 0 or greater than 12
+        if (month < 0 || month > 12) {
+            return 1;
+        }
+
+        // Checks if day is greater than the number of days in a year
+        if (day > dayCheck) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
 int checkLeapYear(int year) {
     int leapYear = 0;
+
+    // Checks if the year is divisible by 4
     if (year % 4 == 0) {
+        // Checks if the year is divisible by 100
         if (year % 100 == 0) {
+            // If it is divisible by 100, check if it is divisible by 400
             if (year % 400 == 0) {
                 leapYear = 1;
             }
-        } else {
+        }
+
+        // If it is not divisible by 100, then it must be a leap year
+        else {
             leapYear = 1;
         }
     }
@@ -146,6 +217,10 @@ int checkLeapYear(int year) {
 }
 
 int roundOwn(float input) {
+
+    // Round offs the input. if the input + 0.5 when round down is greater than
+    // input when round down, then that means that the decimal is >=0.5, which
+    // means it should be round up, otherwise it isn't
     if ((int)(input + 0.5) > (int)input) {
         input += 0.5;
     }
